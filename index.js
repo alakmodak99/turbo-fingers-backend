ee098b0c32643f6b157ad11cbff07ca535bce5a7
MohdTazammul <tazammul1683@gmail.com>
2022-09-15 03:22:07 +0530

    server: Added server configurations

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const userController = require("./controllers/User.controller");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/user', userController)
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is running on PORT ", PORT);
+})
