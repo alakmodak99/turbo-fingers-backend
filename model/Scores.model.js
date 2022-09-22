f5a3ca5ce6cbd74a89685124cd2a3b141f5173de
MohdTazammul <tazammul1683@gmail.com>
2022-09-22 22:09:28 +0530

    Modiefied schema of Scores model

--- model/Scores.model.js
+++ model/Scores.model.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose")
 const Score = mongoose.Schema({
     speed : {type:Number, required:true},
     accuracy : {type:String, required:true},
+    totalSeconds : {type:Number, required:true},
     totalWords : {type:Number, required:true},
     totalChars : {type:Number, required:true},
     totalWrongChars : {type:Number, required:true},
     freqOfWrongChars : {type:Object, default : {}},
-    level : {type:String, required:true},
-    category : {type:String, required:true},
-    paragraph : {type:String, required:true},
+    feedback : {type:String, },
+    paragraphTitle : {type:String, required:true},
     user : {type:mongoose.Types.ObjectId, ref:"user"}
 },{
-    timestamps:true,
-    versionkey:false
+    versionKey:false,
+    timestamps:true
 }) 
 
 module.exports = new mongoose.model("score", Score)
