89497ced458f5ec085c9e4d6e57014db0faefad7
MohdTazammul <tazammul1683@gmail.com>
2022-09-25 23:32:31 +0530

    Added one more column for username in User model

--- model/User.model.js
+++ model/User.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const User = mongoose.Schema({
     email:{type:String, required:true},
     name:{type:String, required: true},
+    username:{type:String, default:""},
     image:{type:"String"},
     authProvider:{type:String, default:"google"} 
 },
