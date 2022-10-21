49101a2390156113547af310ef642c97c4ca0ec0
MohdTazammul <tazammul1683@gmail.com>
2022-10-22 01:47:21 +0530

    server

--- configs/db.js
+++ configs/db.js
@@ -4,4 +4,5 @@ const mongoose = require("mongoose")
 
 module.exports = () =>{
     mongoose.connect('mongodb+srv://tazammul:turbo-fingers@cluster0.lhn5xru.mongodb.net/turbo-fingers?retryWrites=true&w=majority')
+    
 }
